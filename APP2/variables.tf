43b854b00eb67f4ac559635fe7d082644ba75150
Alberto Caycho <parritocloud@outlook.com>
2024-06-27 23:22:27 -0500

    push APP2 to main module2 v2

---
+++ APP2/variables.tf
@@ -0,0 +1,29 @@
+variable "prefix" {
+
+}
+variable "location" {
+
+}
+variable "Owner" {
+
+}
+
+variable "account_tier" {
+
+}
+variable "account_replication_type" {
+
+}
+variable "subnet_name" {
+
+}
+variable "allowed_ip_address" {
+
+}
+variable "tenant_id" {
+
+}
+variable "client_id" {
+
+  sensitive = true
+}
