b6c53972bb3b9f5f6ce0213965f38ea86ef9b44c
Alberto Caycho <parritocloud@outlook.com>
2024-06-27 22:59:10 -0500

    push APP2 to main module2 v2

--- main.tf
+++ main.tf
@@ -1,27 +1,9 @@
 module "app" {
-  source = "./app2"
+  source = "APP2"
 
   prefix   = var.prefix
   location = var.location
   Owner    = var.Owner
 
-  # Pasa otras variables si es necesariotesttt
-}
 
-# Definición de variables en el root module
-variable "prefix" {
-  description = "Prefix for resources"
-  type        = string
 }
-
-variable "location" {
-  description = "Azure location for resources"
-  type        = string
-}
-
-variable "Owner" {
-  description = "Owner of the resources"
-  type        = string
-}
-
-# Pueden ser definidas más variables o configuraciones necesarias
