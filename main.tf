edf46c820cde4412daca64d66af8d2b35300c20e
Alberto Caycho <parritocloud@outlook.com>
2024-06-27 23:08:32 -0500

    push APP2 to main module2 v2

--- main.tf
+++ main.tf
@@ -1,4 +1,33 @@
 module "app" {
 
-  source = "../APP2"
+  source = "APP2"
+}
+variable "prefix" {
+
+}
+variable "location" {
+
+}
+variable "Owner" {
+
+}
+
+variable "account_tier" {
+
+}
+variable "account_replication_type" {
+
+}
+variable "subnet_name" {
+
+}
+variable "allowed_ip_address" {
+
+}
+variable "tenant_id" {
+
+}
+variable "client_id" {
+
+  sensitive = true
 }
