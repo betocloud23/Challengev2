270e4dfe0b6de79af51335e7af649ef1371ee886
Alberto Caycho <parritocloud@outlook.com>
2024-06-29 09:07:11 -0500

    upload modules

--- main.tf
+++ main.tf
@@ -1,31 +1,48 @@
-module "app3" {
-
-  source = "./APP3"
-
-  account_tier             = var.account_tier
-  location                 = var.location
-  prefix                   = var.prefix3
-  Owner                    = var.Owner
-  subnet_cidr              = var.subnet_cidr
-  client_id                = var.client_id
-  tenant_id                = var.tenant_id
-  allowed_ip_address       = var.allowed_ip_address
-  account_replication_type = var.account_replication_type
-  vnetcidr                 = var.vnetcidr
+module "tranzact_rg" {
+  source = "./tranzact_rg"
 }
 
-module "app2" {
+module "tranzact_vnet" {
+  source              = "./tranzact_vnet"
+  resource_group_name = module.tranzact_rg.name
+  location            = module.tranzact_rg.location
+}
 
-  source = "./APP2"
+module "tranzact_subnet" {
+  source               = "./tranzact_subnet"
+  resource_group_name  = module.tranzact_rg.name
+  virtual_network_name = module.tranzact_vnet.name
+}
 
-  account_tier             = var.account_tier
-  location                 = var.location
-  prefix                   = var.prefix2
-  Owner                    = var.Owner
-  subnet_cidr              = var.subnet_cidr
-  client_id                = var.client_id
-  tenant_id                = var.tenant_id
-  allowed_ip_address       = var.allowed_ip_address
-  account_replication_type = var.account_replication_type
-  vnetcidr                 = var.vnetcidr
+module "tranzact_nic" {
+  source              = "./tranzact_nic"
+  resource_group_name = module.tranzact_rg.name
+  location            = module.tranzact_rg.location
+  subnet_id           = module.tranzact_subnet.id
+}
+
+module "tranzact_nsg" {
+  source              = "./tranzact_nsg"
+  resource_group_name = module.tranzact_rg.name
+  location            = module.tranzact_rg.location
+}
+
+module "tranzact_subnet_nsg" {
+  source                    = "./tranzact_subnet_nsg"
+  subnet_id                 = module.tranzact_subnet.id
+  network_security_group_id = module.tranzact_nsg.id
+}
+
+module "tranzact_keyvault" {
+  source              = "./tranzact_keyvault"
+  resource_group_name = module.tranzact_rg.name
+  location            = module.tranzact_rg.location
+  subnet_id           = module.tranzact_subnet.id
+}
+
+module "tranzact_storage_account" {
+  source              = "./tranzact_storage_account"
+  resource_group_name = module.tranzact_rg.name
+  location            = module.tranzact_rg.location
+  subnet_id           = module.tranzact_subnet.id
 }
