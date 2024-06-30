4d1de35e39818d37f1dc9ee799cce3e775cfca47
Alberto Caycho <parritocloud@outlook.com>
2024-06-30 16:16:32 -0500

    update

--- tranzact_rg/main.tf
+++ tranzact_rg/main.tf
@@ -5,3 +5,11 @@ resource "azurerm_resource_group" "app3tranzact" {
     Owner = var.Owner
   }
 }
+
+resource "azurerm_resource_group" "app2tranzact" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+  tags = {
+    Owner = var.Owner
+  }
+}
